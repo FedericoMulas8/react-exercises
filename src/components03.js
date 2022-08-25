f25234dbcd7b9ac800f7a2387af251a1e7a640c3
Federico <104081583+FedericoMulas8@users.noreply.github.com>
2022-08-25 16:08:30 +0200

    components03

---
+++ src/components03.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    );
+  }
+}
+
+root.render(<App />);
