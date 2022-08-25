45e2689cebb3c7040b4bf7a31e4c0d7e10dffb5a
Federico <104081583+FedericoMulas8@users.noreply.github.com>
2022-08-25 16:25:09 +0200

    components07

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const Message = <p>What a beautiful day!</p>
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Hello, World!
+                </h1>
+                {Message}
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return <HelloWorld />
+    }
+}
+
+root.render(<App />)
