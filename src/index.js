d805d13f9ae955f6388a3bc5449e48f72bb815d8
Federico <104081583+FedericoMulas8@users.noreply.github.com>
2022-08-29 20:18:24 +0200

    state04

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+class CounterDisplay extends React.Component {
+    render() {
+        return <h1>{this.props.count}</h1>
+    }
+}
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            count: this.props.initialValue,
+        }
+
+        setInterval(() => {
+            this.setState({
+                count: this.state.count + this.props.incrementValue
+            })
+        }, this.props.updateValue)
+    }
+
+    render() {
+        return <CounterDisplay count={this.state.count} />
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return <Counter initialValue={5} incrementValue={2} updateValue={1000} />
+        }
+}
+
+root.render(<App />)
