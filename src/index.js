cdb84dfbeb76110d8bfd844f368a81c5d3af5d4a
Federico <104081583+FedericoMulas8@users.noreply.github.com>
2022-09-06 11:51:55 +0200

    lists04

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+class List extends React.Component {
+
+    state = {
+        items: ['Mouse', 'Monitor', 'Keyboard'],
+        newItem: '',
+    }
+
+    handleInputChange = (event) => {
+        const obj = event.target.value
+        this.setState({
+            newItem: obj,
+        })
+    }
+
+    addItem = (event) => {
+        this.setState({
+            items: [...this.state.items, this.state.newItem],
+            newItem: '',
+        })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <ul>
+                    {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
+                </ul>
+                <input value={this.state.newItem} onChange={this.handleInputChange}></input>
+                <button onClick={this.addItem}>Add Item</button>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <List/>
+            </div>
+        )
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App />)
